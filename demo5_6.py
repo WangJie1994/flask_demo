1263dfa877dc2c3bc1b2857b5df0b927b9db45a3
jie <wangjie19940416@icloud.com>
2018-12-16 21:15:27 +0800

    .

--- demo5_6.py
+++ demo5_6.py
@@ -1,1 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy
+import os
 from flask import Flask
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+# print(os.path.dirname(__file__))
+# print(basedir)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# print(app.config['SQLALCHEMY_DATABASE_URI'])
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
